9e3e13ac3829d4ee5f9976571e6be53e23aca34f
Alex Ivanov <zmn.wtr@gmail.com>
2015-07-01 13:57:37 +0300

    First js for gallery

--- app/js/main.js
+++ app/js/main.js
@@ -17,3 +17,43 @@ $.fn.scroller = function () {
 	})
 }
 $('.js-up-button').scroller();
+
+var images = $('.js-slider-list').children(),
+	countImg = images.length,
+	currentImg = 0;
+images.filter(':lt(3)')
+	.addClass('js-slide-active');
+images.filter(':not(.js-slide-active)')
+	.css('opacity', 0);
+$('.js-slider-right').click(function (event) {
+	event.preventDefault();
+	currentImg++;
+	$('.js-slide-active').first().animate({
+			opacity: 0
+		}, 400)
+		.removeClass('js-slide-active');
+	$(images[0]).animate({
+		marginLeft: -35*currentImg +'%'}, 
+		400);
+	$('.js-slide-active').last().next()
+		.animate({
+			opacity: 1
+		}, 400)
+		.addClass('js-slide-active');
+});
+$('.js-slider-left').click(function (event) {
+	event.preventDefault();
+	currentImg--;
+	$('.js-slide-active').last().animate({
+			opacity: 0
+		}, 400)
+		.removeClass('js-slide-active');
+	$(images[0]).animate({
+		marginLeft: -35*currentImg +'%'}, 
+		400);
+	$('.js-slide-active').first().prev()
+		.animate({
+			opacity: 1
+		}, 400)
+		.addClass('js-slide-active');
+});
