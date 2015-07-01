4b0b1080535bfd4ca6f2dfcf53570c35725d902a
Alex Ivanov <zmn.wtr@gmail.com>
2015-07-01 05:40:07 +0300

    Add fonts, full html/css and a bit js

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+$.fn.scroller = function () {
+	var button = this;
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 0) {
+			button.fadeIn();
+		} else {
+			button.fadeOut();
+		}
+	});
+	button.click(function(event) {
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0
+			}, 400);
+	})
+}
+$('.js-up-button').scroller();
